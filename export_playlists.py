f05012ff0d52cb2ebfec2782494acb29f87c93c0
Simon Brulhart <simon@brulhart.me>
2018-07-11 17:36:21 +0200

    Move deleted playlists backups elsewhere

--- export_playlists.py
+++ export_playlists.py
@@ -25,12 +25,15 @@ def main():
     token = auth_server.get_token()
     print("Starting export")
     sp = Spotify(auth=token)
+    backup_fnames = set()
     for pl in retrieve_all_items(sp, sp.current_user_playlists()):
         name = pl['name']
+        plname = pl['name'].replace('/','_')
+        backup_fname = f"{plname}_{pl['id']}.json"
+        backup_fnames.add(backup_fname)
+        backup_fpath = pl_folder / backup_fname
 
         # Check if there already is a current backup
-        plname = pl['name'].replace('/','_')
-        backup_fpath = pl_folder / f"{plname}_{pl['id']}.json"
         if backup_fpath.resolve().exists():
             text = backup_fpath.read_text()
             stored_playlist = json.loads(text)
@@ -44,6 +47,15 @@ def main():
         )
         playlist['tracks'] = retrieve_all_items(sp, playlist['tracks'])
         backup_fpath.write_text(json.dumps(playlist))
+
+    # Move deleted playlists elsewhere
+    deleted_fpath = pl_folder / 'deleted'
+    deleted_fpath.mkdir(exist_ok=True)
+    all_fnames = set(p.name for p in pl_folder.glob('*.json'))
+    deleted_fnames = all_fnames - backup_fnames
+    for fname in deleted_fnames:
+        print(f'Move playlist backup to deleted folder: {fname}')
+        (pl_folder / fname).replace(deleted_fpath / fname)
 
 
 if __name__ == "__main__":
